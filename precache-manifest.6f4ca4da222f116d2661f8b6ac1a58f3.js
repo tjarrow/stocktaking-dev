f234a5a4170eddde2817259c13e47cacc05e5948
tjarrow <nickita0104@gmail.com>
2019-12-29 14:14:51 +0300

    Updates

---
+++ precache-manifest.6f4ca4da222f116d2661f8b6ac1a58f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331e1ac9869f44ab867536570c2932aa",
+    "url": "/stocktaking-dev/index.html"
+  },
+  {
+    "revision": "b9924cc967d0fa3eda84",
+    "url": "/stocktaking-dev/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "886cd0fea8f449e0822c",
+    "url": "/stocktaking-dev/static/js/2.c228edf2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stocktaking-dev/static/js/2.c228edf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9924cc967d0fa3eda84",
+    "url": "/stocktaking-dev/static/js/main.544b436e.chunk.js"
+  },
+  {
+    "revision": "1671f84da90d27c74512",
+    "url": "/stocktaking-dev/static/js/runtime-main.8e9b228e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stocktaking-dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
